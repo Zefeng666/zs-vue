9ea9dd16cbf7234f67689b37ddab7a1ece7d9080
lzf <liuzefeng218@foxmail.com>
2018-10-19 01:20:23 +0800

    build

--- src/views/sales/getGoods/getGoods.vue
+++ src/views/sales/getGoods/getGoods.vue
@@ -250,7 +250,7 @@ export default {
           "paySign": paySign //微信签名 
         },
         function(res){
-          console.log(res);
+          alert(res);
           
           if(res.err_msg == "get_brand_wcpay_request:ok" ){
           // 使用以上方式判断前端返回,微信团队郑重提示：
