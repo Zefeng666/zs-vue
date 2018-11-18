14751e0627d20d1e7d3b28092483af5d9e5a7ea7
zefeng <245613360@qq.com>
2018-11-18 19:27:57 +0800

    build

--- src/views/sales/getGoods/getGoods.vue
+++ src/views/sales/getGoods/getGoods.vue
@@ -224,6 +224,8 @@ export default {
             this.onBridgeReady(data.data.appId, data.data.nonceStr, data.data.package, data.data.paySign, data.data.timeStamp)   
           } else {
             this.$vux.toast.text("添加线下支付订单成功~", "top");
+            this.queryOrder();
+            this.index = 1;
           }
           this.showToast = false;
         } else {
