93bca150ead9f28dd9e7a6493126df2d5966ffde
zefeng <245613360@qq.com>
2018-12-08 19:27:46 +0800

    build

--- src/views/mine/qrcode/qrcode.vue
+++ src/views/mine/qrcode/qrcode.vue
@@ -6,7 +6,7 @@
       <p>{{userInfo.username}}</p>
       <p>{{userInfo.vipLevel | formatVipLevel}}</p>
     </div>
-    <qrcode :value="'http://haoyi.tlong.tv/register?inviteCode=' + userInfo.code" type="img" :size="200"></qrcode>
+    <qrcode :value="'https://haoyi.houseselected.com/register?inviteCode=' + userInfo.code" type="img" :size="200"></qrcode>
   </div>
 </template>
 
